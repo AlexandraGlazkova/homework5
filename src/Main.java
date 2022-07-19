4432889c5aaa610f2cb8cde7b55678f02891b0d7
GlakovaA <22109@bk.ru>
2022-07-20 02:21:59 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+ //       ## 1 задача
+//        Объявите три массива:
+//        1. Целочисленный массив, заполненный тремя цифрами – 1, 2 и 3 – с помощью ключевого
+//        слова new.
+        int [] weight=new int[3];
+        weight[0]=1;
+        weight[1]=2;
+        weight[2]=3;
+         for (int i = 0; i < 3; i++) {
+             System.out.println(weight[i]);
+         }
+//       2. Массив, в котором можно хранить три дробных числа – 1.57, 7.654, 9.986 –
+//        сразу заполнив его значениями.
+        double [] array = {1.57, 7.654, 9.986};
+         for (int k =0; k<array.length; k++) {
+            System.out.println(array[k]);
+        }
+//        3. Произвольный массив – тип и количество данных определите сами. Самостоятельно выберите
+//        способ создания массива: с помощью ключевого слова или сразу заполненный элементами.
+        int [] food = new int [5];
+         food[2] =59;
+         food[3]=96;
+         food[0]=520;
+        for (int t=0; t<5;t++) {
+            System.out.println(food[t]);}
     }
 }
